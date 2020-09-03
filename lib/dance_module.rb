d2f28158dd1196feedd8cef37fc69a356cd585d2
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 17:10:45 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,27 +1,17 @@
-module FancyDance
-  module InstanceMethods
-
-    def twirl
-      "I'm twirling!"
-    end
-
-    def jump
-      "Look how high I'm jumping!"
-    end
-
-    def pirouette
-      "I'm doing a pirouette"
-    end
-
-    def take_a_bow
-      "Thank you, thank you. It was a pleasure to dance for you all."
-    end
+module Dance
+  def twirl
+    "I'm twirling!"
   end
 
-  module ClassMethods
+  def jump
+    "Look how high I'm jumping!"
+  end
 
-    def metadata
-      "This class produces objects that love to dance."
-    end
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
   end
 end
