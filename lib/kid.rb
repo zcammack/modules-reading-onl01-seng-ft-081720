cf6ea3235a35e72acfd68fb182889061e31e6ca1
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 17:12:20 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,8 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   extend MetaDancing
   include Dance
 
